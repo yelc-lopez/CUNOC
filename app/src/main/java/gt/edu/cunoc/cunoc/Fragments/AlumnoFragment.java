89ab90f1bd51cab8116b80f32ee112cde7908683
yelc556 <yelc556@gmail.com>
2017-12-12 14:45:36 -0600

    Actualizacion de Noticias

--- app/src/main/java/gt/edu/cunoc/cunoc/Fragments/AlumnoFragment.java
+++ app/src/main/java/gt/edu/cunoc/cunoc/Fragments/AlumnoFragment.java
@@ -1,19 +1,25 @@
 package gt.edu.cunoc.cunoc.Fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import gt.edu.cunoc.cunoc.DetallesNoticiaActivity;
 import gt.edu.cunoc.cunoc.R;
+import gt.edu.cunoc.cunoc.UserActivity;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AlumnoFragment extends Fragment {
 
+
+    Button button1, button2;
 
     public AlumnoFragment() {
         // Required empty public constructor
@@ -24,7 +30,26 @@ public class AlumnoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alumno, container, false);
+        View view =  inflater.inflate(R.layout.fragment_alumno, container, false);
+        button1 = view.findViewById(R.id.boton1);
+        button2 = view.findViewById(R.id.boton2);
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(),UserActivity.class));
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(),DetallesNoticiaActivity.class));
+            }
+        });
+
+        return view;
     }
 
 }
